a0aaabeae35b84d36a087d06d857cce10a5978fa
Bravi <bravilogy@gmail.com>
2021-05-30 22:56:09 +0100

    remove gatsby-plugin-netlify-functions

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'twisted-hologram',
   },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-netlify-functions`,
-      options: {
-        functionsSrc: `${__dirname}/src/functions`,
-        functionsOutput: `${__dirname}/functions`,
-      },
-    },
-  ],
+  plugins: [],
 };
